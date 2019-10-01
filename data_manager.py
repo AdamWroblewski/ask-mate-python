89872c5b4dc86d9942a6fa97d2fe65e1e49349ad
Adam <adamwroblewski.1993@gmail.com>
2019-10-01 22:13:46 +0200

    get_post_title return post titles

--- data_manager.py
+++ data_manager.py
@@ -4,10 +4,8 @@ import connection
 def get_post_title():
     posts = connection.read_csv_data('sample_data/question.csv')
 
-    title_list = []
-    FILE_WITHOUT_HEADERS = 1
-    TITLE_INDEX = 4
-    for title in posts[FILE_WITHOUT_HEADERS:]:
-        title_list.append(title[TITLE_INDEX])
+    titles = []
+    for post in posts:
+        titles.append(post['title'])
 
-    return title_list
+    return titles
