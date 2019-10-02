43a37511cfa1050139347fa49c90eaacebc806c8
Adam <adamwroblewski.1993@gmail.com>
2019-10-02 11:31:28 +0200

    serve all data to template

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 import data_manager
+import util
 
 app = Flask(__name__)
 
@@ -7,8 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/list')
 def list_route():
-    titles = data_manager.get_post_title()
-    return render_template('list-questions.html', titles=titles)
+    questions = data_manager.get_all_questions()
+    sorted_titles = util.sort_by_time_submission(questions)
+    return render_template('list-questions.html', questions=sorted_titles)
 
 
 @app.route('/question/<question_id>')
