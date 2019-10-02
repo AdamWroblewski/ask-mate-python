a66d7878cd13f4aa19a2f08488b38b1b79081ca5
Adam <adamwroblewski.1993@gmail.com>
2019-10-02 14:48:38 +0200

    Implement conver dictionary elements into int

--- util.py
+++ util.py
@@ -1,5 +1,18 @@
 from operator import itemgetter
-import data_manager
+from datetime import datetime
+
 
 def sort_by_time_submission(posts):
     return sorted(posts, key=itemgetter('submission_time'), reverse=True)
+
+
+def convert_timestamp_to_date(timestamp):
+    return datetime.fromtimestamp(timestamp)
+
+
+def conver_to_int(dictionary, *args):
+    for arg in args:
+        if arg not in dictionary.keys():
+            raise KeyError('Wrong key.')
+        dictionary[arg] = int(dictionary[arg])
+
