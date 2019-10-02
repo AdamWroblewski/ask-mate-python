9bf01efb3bc615757508a29afa419f5312bf999b
Adam <adamwroblewski.1993@gmail.com>
2019-10-02 20:49:56 +0200

    merge conflict

--- data_manager.py
+++ data_manager.py
@@ -17,7 +17,7 @@ def get_all_questions():
     for post in posts:
         try:
             util.conver_to_int(post, 'id', 'submission_time', 'view_number', 'vote_number')
-        except ValueError:
+        except:
             return None
 
     return posts
@@ -35,8 +35,9 @@ def get_all_answers():
     answers = connection.read_csv_data('sample_data/answer.csv')
     for answer in answers:
         try:
-            util.conver_to_int(answer, 'id', 'submission_time', 'view_number', 'question_id')
+            util.conver_to_int(answer, 'id', 'submission_time', 'view_number', 'vote_number', 'question_id')
         except ValueError:
             return None
 
     return answers
+
