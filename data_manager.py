c6731270004b4bcfe81321de2f63367314d63846
Adam <adamwroblewski.1993@gmail.com>
2019-10-02 11:58:56 +0200

    Read question by id

--- data_manager.py
+++ data_manager.py
@@ -13,3 +13,11 @@ def get_post_title():
 
 def get_all_questions():
     return connection.read_csv_data('sample_data/question.csv')
+
+
+def get_question_by_id(question_id):
+    posts = connection.read_csv_data('sample_data/question.csv')
+
+    for post in posts:
+        if post['id'] == question_id:
+            return post
