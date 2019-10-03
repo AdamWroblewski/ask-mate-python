9787703e45a258a1c4ca6fc76ef8122a64fbb6b3
Adam <adamwroblewski.1993@gmail.com>
2019-10-03 13:45:37 +0200

    Create list with headers, append post to csv

--- connection.py
+++ connection.py
@@ -1,6 +1,9 @@
 import csv
 
 
+QUESTION_HEADERS = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+
+
 def read_csv_data(file_name):
     post_list = []
     with open(file_name) as posts:
@@ -10,3 +13,10 @@ def read_csv_data(file_name):
             post_list.append(post)
 
     return post_list
+
+
+def append_csv_data(file_name, data):
+    with open(file_name, 'a') as file:
+        writer = csv.DictWriter(file, fieldnames=QUESTION_HEADERS)
+
+        writer.writerow(data)
