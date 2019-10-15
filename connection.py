60248705c04db728bdb74fd0d3f4af7ae7517d62
Adam <adamwroblewski.1993@gmail.com>
2019-10-15 19:15:30 +0200

    Delete unused function

--- connection.py
+++ connection.py
@@ -1,29 +1,6 @@
-import csv
 import os
 import psycopg2
 import psycopg2.extras
-
-
-QUESTION_HEADERS = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
-ANSWER_HEADERS = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
-
-
-def read_csv_data(file_name):
-    post_list = []
-    with open(file_name) as posts:
-        reader = csv.DictReader(posts)
-
-        for post in reader:
-            post_list.append(post)
-
-    return post_list
-
-
-def append_csv_data(file_name, data, fieldnames):
-    with open(file_name, 'a') as file:
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-
-        writer.writerow(data)
 
 
 def create_connection_string():
@@ -54,7 +31,6 @@ def open_database():
 def connection_handler(function):
     def wrapper(*args, **kwargs):
         connection = open_database()
-        # we set the cursor_factory parameter to return with a RealDictCursor cursor (cursor which provide dictionaries)
         dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         ret_value = function(dict_cur, *args, **kwargs)
         dict_cur.close()
